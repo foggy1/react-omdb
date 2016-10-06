da3ae860fb57b6c55c758612a62ce33e6dd7b7d1
Austin Lanari <email": null>
2016-10-06 12:39:57 -0400

    Add initialization of state; remove invalid reference to data; get page loading with default info

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,18 +1,12 @@
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movie_info: []
+    };
+  }
   componentDidMount() {
-    $('.btn').click(function(e) {
-      e.preventDefault();
-      data = $('.movieSearch').serialize();
-      $.ajax({
-        dataType: "json",
-        url: "http://www.omdbapi.com/?",
-        data: data,
-      })
-      .done(function(response) {
-        debugger;
-
-      })
-    })
+    this.setState({movie_info: {"Title":"Fight Club","Year":"1999","Rated":"R","Released":"15 Oct 1999","Runtime":"139 min","Genre":"Drama","Director":"David Fincher","Writer":"Chuck Palahniuk (novel), Jim Uhls (screenplay)","Actors":"Edward Norton, Brad Pitt, Meat Loaf, Zach Grenier","Plot":"An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.","Language":"English","Country":"USA, Germany","Awards":"Nominated for 1 Oscar. Another 10 wins & 31 nominations.","Poster":"http://ia.media-imdb.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg","Metascore":"66","imdbRating":"8.8","imdbVotes":"1,359,025","imdbID":"tt0137523","Type":"movie","Response":"True"}})
   }
   render() {
 
@@ -25,7 +19,7 @@ class App extends React.Component {
             <MovieSearch />
           </div>
           <div className="col-sm-4 col-sm-offset-2">
-            <MovieDisplay />
+            <MovieDisplay movie_info={this.state.movie_info}/>
           </div>
         </div>
       </div>
--- app/assets/javascripts/components/movie_display.es6.jsx
+++ app/assets/javascripts/components/movie_display.es6.jsx
@@ -1,20 +1,33 @@
 class MovieDisplay extends React.Component {
   render() {
-    // let { title, year, img_src, plot } = this.props.data;
-    let movie = {
-      title: 'Fight Club',
-      year: '1999',
-      img_src: 'http://ia.media-imdb.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-      plot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.'
-    }
+    // let movie = {
+    //   Title: 'Fight Club',
+    //   Year: '1999',
+    //   Posterc: 'http://ia.media-imdb.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+    //   {lot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.'
+    // }
   
-    let { title, year, img_src, plot } = movie;
+    let { Title, Year, Poster, Plot } = this.props.movie_info;
     return(
       <div>
-        <h2 className="h2">{title} ({year})</h2>
-        <img className="img-fluid" src={img_src} />
-        <p>Plot: {plot}</p>
+        <h2 className="h2">{Title} ({Year})</h2>
+        <img className="img-fluid" src={Poster} />
+        <p>Plot: {Plot}</p>
       </div>
       )
   }
 }
+
+
+    $('.btn').click(function(e) {
+      e.preventDefault();
+      data = $('.movieSearch').serialize();
+      $.ajax({
+        dataType: "json",
+        url: "http://www.omdbapi.com/?",
+        data: data,
+      })
+      .done(function(response) {
+        this.setState({movie_info: response});
+      })
+    })
