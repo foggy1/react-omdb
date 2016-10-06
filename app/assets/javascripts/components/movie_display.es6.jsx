d24cb3d213d57539de5e7f28c10b9b8113647b13
Austin Lanari <email": null>
2016-10-06 11:55:40 -0400

    Move movie display into component

---
+++ app/assets/javascripts/components/movie_display.es6.jsx
@@ -0,0 +1,11 @@
+class MovieDisplay extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="h2">Fight Club (1999)</h2>
+        <img className="img-fluid" src="http://ia.media-imdb.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" />
+        <p>Plot: An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.</p>
+      </div>
+      )
+  }
+}
